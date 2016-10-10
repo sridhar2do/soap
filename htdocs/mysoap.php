6190317cd353bb00d6ef13ec49d6a16041581648
sridhar2do <sridhar2do@gmail.com>
2016-10-10 20:59:34 +0530

    Add Server

---
+++ htdocs/mysoap.php
@@ -0,0 +1,10 @@
+<?php
+
+require('api/Events.php');
+
+$options = array("uri" => "http://localhost");
+
+$server = new SoapServer(null, $options);
+$server->setClass('Events');
+$server->handle();
+
